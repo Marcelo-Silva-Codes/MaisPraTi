6df8f1af09ba5deaad9daaa4389e8f6565220c1c
Marcelo silva <marcelomxm26@gmail.com>
2026-08-19 11:02:08 -0300

    feat: aulas 32 e 33

---
+++ Modulo_3/Clarim-diario/src/App.jsx
@@ -0,0 +1,37 @@
+import Header from "./components/Header/Header.jsx";
+import NewsCard from "./components/NewsCard/NewsCard.jsx";
+import { noticias } from "./data/noticias.js";
+import './App.css'
+
+function App() {
+  const [manchete, ...demais] = noticias;
+
+  return (
+    <>
+      <Header />
+
+      <main className='container'>
+        <section className='manchete'>
+          <NewsCard
+            categoria={manchete.categoria}
+            titulo={manchete.titulo}
+            resumo={manchete.resumo}
+          />
+        </section>
+
+        <section className='grade'>
+          {demais.map((noticia) => (
+            <NewsCard
+              key={noticia.id}
+              categoria={noticia.categoria}
+              titulo={noticia.titulo}
+              resumo={noticia.resumo}
+            />
+          ))}
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
